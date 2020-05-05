9ddaa87bcd574d41505e52b2353922d09b67f11e
Miguel Boanova <mgl.deadly@gmail.com>
2020-05-05 00:15:30 -0300

    :art: change class name to libs default

--- src/user/user.input.ts
+++ src/user/user.input.ts
@@ -1,8 +1,9 @@
 import { InputType, ObjectType, Int, Field, GraphQLISODateTime, ID } from '@nestjs/graphql';
 import { FilterableField } from '@nestjs-query/query-graphql';
+import { UserEntity } from './user.entity';
 
 @ObjectType('User')
-export class User {
+export class UserDTO  implements Partial<UserEntity> {
   @FilterableField(() => ID)
   id!: number;
 
